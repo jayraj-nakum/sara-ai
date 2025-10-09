77cdfc18bdb8781166f86ed97dc3c86f108890d1
jayrajnakum-at <jayraj.nakum@avestatechnologies.com>
2025-10-09 17:39:38 +0530

    add ai to ai

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const micBtn = document.getElementById("mic-btn");
 const sendBtn = document.getElementById("send-btn");
 const textInput = document.getElementById("text-input");
 const chatBody = document.getElementById("chat-body");
-const geminiApiKey = "AIzaSyBSy5lyVCFbut5EqmhCqIgRgrurhdjKHrA"; // Replace if neede
+const geminiApiKey = "jKHrA"; // Replace if neede
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
